b2df678a0fd3a50a26b285efe529e715c8d3f50f
neelk <neelk.brainerhub@gmail.com>
2025-08-26 12:14:43 +0530

    SPEED CHANGES

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import AuthProvider from "../component/AuthProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body><AuthProvider>{children}</AuthProvider></body>
+      <SpeedInsights />
     </html>
   );
 }
